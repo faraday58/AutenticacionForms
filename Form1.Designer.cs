8590156207717cdf7fe967e066ba30d2d478cab2
armando <jarodriguezvera@gmail.com>
2024-04-03 12:51:27 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace AutenticacionForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(267, 215);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 53);
+            this.btnEntrar.TabIndex = 0;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(82, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuario";
+            // 
+            // txtbUsuario
+            // 
+            this.txtbUsuario.Location = new System.Drawing.Point(233, 87);
+            this.txtbUsuario.Name = "txtbUsuario";
+            this.txtbUsuario.Size = new System.Drawing.Size(216, 22);
+            this.txtbUsuario.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(82, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtbPassword
+            // 
+            this.txtbPassword.Location = new System.Drawing.Point(233, 147);
+            this.txtbPassword.Name = "txtbPassword";
+            this.txtbPassword.PasswordChar = '*';
+            this.txtbPassword.Size = new System.Drawing.Size(216, 22);
+            this.txtbPassword.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 330);
+            this.Controls.Add(this.txtbPassword);
+            this.Controls.Add(this.txtbUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEntrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbPassword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using UsuarioLibrary;
+using System.Windows.Forms;
+using ReproductorMusica_G2_2024_II;
+
+namespace AutenticacionForms
+{
+    public partial class Form1 : Form
+    {
+        ArrayList usuarios;
+        public Form1()
+        {
+            InitializeComponent();
+            usuarios = new ArrayList();
+            AgregarUsuarios();
+        }
+
+
+        private void AgregarUsuarios()
+        {
+            usuarios.Add(new Usuario("armando" ,"12345"));
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            string usuario = txtbUsuario.Text;
+            string password = txtbPassword.Text;
+
+            var usuarioPass =(Usuario)usuarios[0];
+            if( usuario ==  usuarioPass.Nombre && 
+                password == usuarioPass.Password )
+            {
+               
+                FormReproductor formReproductor = new FormReproductor();
+
+                formReproductor.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Usuario o Contraseña incorrecto");
+            }
+
+
+
+        }
+    }
+}
